1118627e262660c67e4b6160c9ea4becfd60bfa5
HungBay <nguyen.hhung97@gmail.com>
2021-11-12 19:54:25 +0700

    refs #2 create nodemon and eslint

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 4200;
 
-app.get('/', (req: any, res: any) => res.send('Hello 3'))
+app.get('/', (req: any, res: any) => res.send('Hello 4'))
 
 app.listen(port)
